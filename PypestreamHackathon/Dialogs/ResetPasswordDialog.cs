35bb06e07f2f194e40cff1d2d3bd1c78e8c9053a
Richard diZerega <richdizz@outlook.com>
2017-03-09 13:24:37 -0600

    updated comments

--- PypestreamHackathon/Dialogs/ResetPasswordDialog.cs
+++ PypestreamHackathon/Dialogs/ResetPasswordDialog.cs
@@ -31,7 +31,7 @@ namespace PypestreamHackathon.Dialogs
             {
                 var alias = await textResult;
 
-                // Check if the user has a mobile number in AD...we can only reset users with mobiles
+                // Can only reset passwords for people with mobile numbers in AAD
                 var mobile = await getMobile(alias);
                 if (String.IsNullOrEmpty(mobile))
                 {
