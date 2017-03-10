6f6334dbba082e41e930671284c473f07407e022
Richard diZerega <richdizz@outlook.com>
2017-03-09 18:41:51 -0600

    Added oxford stuff for face recognition

--- PypestreamHackathon/Dialogs/UpdatePictureDialog.cs
+++ PypestreamHackathon/Dialogs/UpdatePictureDialog.cs
@@ -10,6 +10,9 @@ using System.Web;
 using System.IO;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using Microsoft.ProjectOxford.Face;
+using System.Web;
+using System.Net;
 
 namespace PypestreamHackathon.Dialogs
 {
@@ -44,21 +47,36 @@ namespace PypestreamHackathon.Dialogs
                         var attachment = attachments.FirstOrDefault();
 
                         // TODO: get stream of the attachment
-                        var newPicStream = "";
+                        var newPicContent = attachment.ContentUrl;
+                        HttpClient client = new HttpClient();
+                        var response = await client.GetAsync(newPicContent);
+                        var newPicStream = await response.Content.ReadAsStreamAsync();
+                        byte[] newBytes = new byte[newPicStream.Length];
+                        newPicStream.Read(newBytes, 0, newBytes.Length);
+                        newPicStream = new MemoryStream(newBytes);
 
                         // Get the stream of the current picture
-                        var originalPicStream = await getProfilePic(attachmentsContext.ConversationData.Get<string>("AccessToken"));
+                        var bytes = await getProfilePic(attachmentsContext.ConversationData.Get<string>("AccessToken"));
+                        var oldPicStream = new MemoryStream(bytes, true);
 
-                        // TODO: Check if new picture is a close enough match to the original pic
-
-                        //HACK
-                        attachmentsContext.Done("Sorry, the picture you uploaded does not resemble you.");
+                        double confidence = await GetScoreForTwoFaces(newPicStream, oldPicStream);
+                        if (confidence >= 75)
+                        {
+                            //change picture
+                            newPicStream = new MemoryStream(newBytes);
+                            await updateProfilePic(attachmentsContext.ConversationData.Get<string>("AccessToken"), newPicStream);
+                            attachmentsContext.Done($"Great! We're {confidence}% confident that the picture you uploaded is of yourself. We have updated your profile picture.");
+                        }
+                        else
+                        {
+                            attachmentsContext.Done($"Sorry, we are only {confidence}% confident that the picture you uploaded is you.");
+                        }
                     }, "Please upload a new profile photo");
                 }
             }, context.Activity, CancellationToken.None);
         }
 
-        private async Task<Stream> getProfilePic(string token)
+        private async Task<byte[]> getProfilePic(string token)
         {
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
@@ -67,7 +85,10 @@ namespace PypestreamHackathon.Dialogs
             {
                 if (response.IsSuccessStatusCode)
                 {
-                    return await response.Content.ReadAsStreamAsync();
+                    var stream = await response.Content.ReadAsStreamAsync();
+                    byte[] bytes = new byte[stream.Length];
+                    stream.Read(bytes, 0, bytes.Length);
+                    return bytes;
                 }
                 else
                     return null;
@@ -91,6 +112,45 @@ namespace PypestreamHackathon.Dialogs
                 {
                     //TODO
                 }
+            }
+        }
+        public static async Task<double> GetScoreForTwoFaces(Stream img1, Stream img2)
+        {
+            try
+            {
+                FaceServiceClient client = new FaceServiceClient("f877a6f32c314fd2b6726229f9b3a81a");
+                var faces1 = await client.DetectAsync(img1);
+                var faces2 = await client.DetectAsync(img2);
+
+                if (faces1 == null || faces2 == null)
+                {
+                    var x = 1;
+                    //return Json(new { error = "Error: It looks like we can't detect faces in one of these photos..." });
+                }
+                if (faces1.Count() == 0 || faces2.Count() == 0)
+                {
+                    var x = 1;
+                    //return Json(new { error = "Error: It looks like we can't detect faces in one of these photos..." });
+                }
+                if (faces1.Count() > 1 || faces2.Count() > 1)
+                {
+                    var x = 1;
+                    //return Json(new { error = "Error: Each photo must have only one face. Nothing more, nothing less..." });
+                }
+                var res = await client.VerifyAsync(faces1[0].FaceId, faces2[0].FaceId);
+                double score = 0;
+                if (res.IsIdentical)
+                    score = 100;
+                else
+                {
+                    score = Math.Round((res.Confidence / 0.5) * 100);
+                }
+
+                return score;
+            }
+            catch (Exception ex)
+            {
+                return 0;
             }
         }
     }
