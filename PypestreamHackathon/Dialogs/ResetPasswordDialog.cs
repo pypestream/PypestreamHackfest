227ec6a135bd1165fe408deeea565d13bc470c8e
Richard diZerega <richdizz@outlook.com>
2017-03-09 20:13:02 -0600

    update to password reset and publishing settings

--- PypestreamHackathon/Dialogs/ResetPasswordDialog.cs
+++ PypestreamHackathon/Dialogs/ResetPasswordDialog.cs
@@ -2,6 +2,7 @@
 using Microsoft.Bot.Builder.Luis.Models;
 using Microsoft.Bot.Connector;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -9,7 +10,9 @@ using System.Configuration;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Security.Cryptography;
 using System.Security.Cryptography.X509Certificates;
+using System.Text;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -30,6 +33,7 @@ namespace PypestreamHackathon.Dialogs
             PromptDialog.Text(context, async (IDialogContext textContext, IAwaitable<string> textResult) =>
             {
                 var alias = await textResult;
+                textContext.ConversationData.SetValue<string>("Alias", alias);
 
                 // Can only reset passwords for people with mobile numbers in AAD
                 var mobile = await getMobile(alias);
@@ -40,14 +44,47 @@ namespace PypestreamHackathon.Dialogs
                 else
                 {
                     //TODO: all validation stuff
-                    textContext.Done("TODO: Please paste the code we sent your mobile phone");
+                    mobile = mobile.Replace(" ", "");
+                    var magicNumber = GenerateRandomNumber();
+                    textContext.ConversationData.SetValue<int>("ResetMagicNumber", magicNumber);
+                    Helpers.TwilioHelper.sendSMS(mobile, $"Pypestream: your code is " + magicNumber);
+                    PromptDialog.Text(textContext, async (IDialogContext mnContext, IAwaitable<string> mnResult) =>
+                    {
+                        var code = await mnResult;
+                        if (Convert.ToInt32(code) == mnContext.ConversationData.Get<int>("ResetMagicNumber"))
+                        {
+                            // Reset the password for the user
+                            var randomPassword = System.Web.Security.Membership.GeneratePassword(10, 1);
+                            await resetPassword(mnContext.ConversationData.Get<string>("Alias"), randomPassword);
+                            mnContext.Done($"Your password has been reset to ${randomPassword} and you will be forced to change it after logging in");
+                        }
+                        else
+                        {
+                            // Error...codes didn't match
+                            mnContext.Done("ERROR: Your code did not match the code we sent");
+                        }
+                    }, "Please paste the code Pypestream just sent to your mobile phone");
                 }
 
 
             }, "Locked out huh? I can help you reset your password...what is your alias?");
         }
 
-        private async Task<AuthenticationResult> getAppOnlyToken()
+        private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+        private int GenerateRandomNumber()
+        {
+            int number = 0;
+            byte[] randomNumber = new byte[1];
+            do
+            {
+                rngCsp.GetBytes(randomNumber);
+                var digit = randomNumber[0] % 10;
+                number = number * 10 + digit;
+            } while (number.ToString().Length < 6);
+            return number;
+        }
+
+        private async Task<AuthenticationResult> getAppOnlyToken(string resource)
         {
             AuthenticationContext authenticationContext = new AuthenticationContext("https://login.microsoftonline.com/rzna.onmicrosoft.com", false);
 
@@ -64,12 +101,34 @@ namespace PypestreamHackathon.Dialogs
                 X509KeyStorageFlags.MachineKeySet |
                 X509KeyStorageFlags.PersistKeySet);
             ClientAssertionCertificate cac = new ClientAssertionCertificate(ConfigurationManager.AppSettings["aad:ClientId"], cert);
-            return await authenticationContext.AcquireTokenAsync("https://graph.microsoft.com", cac);
+            return await authenticationContext.AcquireTokenAsync(resource, cac);
+        }
+
+        private async Task resetPassword(string alias, string password)
+        {
+            var token = await getAppOnlyToken("https://graph.windows.net");
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.AccessToken);
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var data = JObject.Parse("{ \"passwordProfile\": { \"password\": \"" + password + "\", \"forceChangePasswordNextLogin\": false }}");
+            var uri = $"https://graph.windows.net/myorganization/users/{alias}?api-version=1.6";
+            var request = new HttpRequestMessage(new HttpMethod("PATCH"), uri)
+            {
+                Content = new StringContent(JsonConvert.SerializeObject(data), Encoding.UTF8, "application/json")
+            };
+            using (var response = await client.SendAsync(request))
+            {
+                //TODO: this will fail...need to look at admin consent or keeping refresh token of a directory admin
+                if (response.IsSuccessStatusCode)
+                {
+                }
+            }
         }
 
         private async Task<string> getMobile(string alias)
         {
-            var token = await getAppOnlyToken();
+            var token = await getAppOnlyToken("https://graph.microsoft.com");
 
             HttpClient client = new HttpClient();
             client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token.AccessToken);
